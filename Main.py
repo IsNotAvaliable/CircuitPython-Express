d6f0cb2ace097822db1ce538f7fb494f0e6ae2a7
IsNotAvaliable <111406232+IsNotAvaliable@users.noreply.github.com>
2023-02-02 21:51:13 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 # Write your code here :-)
 import time
 from adafruit_circuitplayground import cp
+
 def buttons():
     if cp.button_b:
         cp.play_tone(294, 1)
     if cp.button_a:
         cp.play_tone(262, 1)
-    if cp.button_ab:
-        cp.play_tone(150, 3)
+        
 while True:
     buttons()
