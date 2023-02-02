914b1b342c57c9127ec4374bc4d037ab66ee86af
IsNotAvaliable <111406232+IsNotAvaliable@users.noreply.github.com>
2023-02-02 22:06:47 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,30 @@
 # Write your code here :-)
 import time
 from adafruit_circuitplayground import cp
-
 def buttons():
     if cp.button_b:
-        cp.play_tone(294, 1)
+        cp.play_tone(500, 1)
     if cp.button_a:
-        cp.play_tone(262, 1)
+        cp.play_tone(100, 1)
+def BRIGHT():
+    if cp.light >= 50:
+        cp.pixels[0] = (255, 0, 0)
+        cp.pixels[1] = (255, 0, 0)
+        cp.pixels[8] = (255, 0, 0)
+        cp.pixels[9] = (255, 0, 0)
+        print("Light:", cp.light)
+        print((cp.light,))
+        time.sleep(0.2)
+        
+    elif cp.light <= 49:
+        cp.pixels[0] = (0, 0, 0)
+        cp.pixels[1] = (0, 0, 0)
+        cp.pixels[8] = (0, 0, 0)
+        cp.pixels[9] = (0, 0, 0)
+        print("Light:", cp.light)
+        print((cp.light,))
+        time.sleep(0.2)
         
 while True:
     buttons()
+    BRIGHT()
